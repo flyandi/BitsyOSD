95f180cdffefca025d690e160a928e62d5c23182
Andreas Schwarz <flyandi@yahoo.com>
2016-09-19 20:34:43 -0700

    Version change

--- bitsyosd/bitsyosd.ino
+++ bitsyosd/bitsyosd.ino
@@ -15,7 +15,7 @@ http://github.com/FPVTools/BitsyOSD
 
 Copyright (c) 2016. All rights reserved.
 Author: Andreas Schwarz (flyandi)
-Version: 1.4.0
+Version: 1.4
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
